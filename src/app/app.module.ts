4629e66831fa4f7b316c8b836679dad48dcb9cc9
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-08 00:00:17 +1000

    add products page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ProductsComponent } from "./products.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
